0957a4ea51a1e16fa0c3a1b41646d5143bca2011
Magomedcoder <info@magomedcoder.ru>
2022-06-19 22:06:02 +0300

    Added LICENSE, .editorconfig, .gitignore and config provider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Console;
+
+/**
+ * Class ConfigProvider
+ * @package PCore\Console
+ * @github https://github.com/pcore-framework/console
+ */
+class ConfigProvider
+{
+
+    public function __invoke(): array
+    {
+        return [
+            'commands' => [
+                'PCore\\Console\Commands\ControllerMakeCommand'
+            ]
+        ];
+    }
+
+}
